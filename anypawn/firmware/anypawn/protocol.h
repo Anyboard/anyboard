cfa6269caa0dd27d25c230458638e3e4df945f08
Simone Mora <morasimone@gmail.com>
2017-01-23 14:15:49 +0100

    fixed protocol

--- anypawn/firmware/anypawn/protocol.h
+++ anypawn/firmware/anypawn/protocol.h
@@ -1,4 +1,4 @@
-// COMMANDS
+// Status commands
 const uint8_t GET_NAME             = 32;
 const uint8_t GET_VERSION          = 33;
 const uint8_t GET_UUID             = 34;
@@ -11,25 +11,29 @@ const uint8_t HAS_RFID             = 71;
 const uint8_t HAS_NFC              = 72;
 const uint8_t HAS_ACCELEROMETER    = 73;
 const uint8_t HAS_TEMPERATURE      = 74;
-
-const uint8_t LED_OFF2             = 128;
-const uint8_t LED_ON2              = 129;
-const uint8_t LED_BLINK            = 130;
 const uint8_t READ_COLOR           = 136;
 
-//EVENTS
-const uint8_t MOVE                 = 194; //C2
-
+//Token-solo events
 const uint8_t TAP				           = 201; //C9
 const uint8_t DOUBLE_TAP		       = 202; //CA
 const uint8_t SHAKE				         = 203; //CB
 const uint8_t TILT                 = 204; //CC
 
-//COMMANDS
+//Token-constraint events
+const uint8_t MOVE                 = 194; //C2 (+ 2nd and 3rd bytes for current and last sector)
+
+//Token-token events
+//TBD
+
+//feedbacks
 const uint8_t COUNT                = 205; //CD
 const uint8_t DISPLAY_X            = 206; //CE
 const uint8_t VIBRATE              = 200; //C8
-const uint8_t DISPLAY_DIGIT		   = 207;
-const uint8_t DISPLAY_W		   	   = 208;
-const uint8_t DISPLAY_UP		   = 209;
-const uint8_t DISPLAY_DOWN		   = 210;
+const uint8_t DISPLAY_DIGIT		     = 207;
+const uint8_t DISPLAY_W		   	     = 208;
+const uint8_t DISPLAY_UP		       = 209;
+const uint8_t DISPLAY_DOWN		     = 210;
+
+const uint8_t LED_OFF2             = 128;
+const uint8_t LED_ON2              = 129;
+const uint8_t LED_BLINK            = 130;
